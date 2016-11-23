5a0ff60a8244506c5381b84b8ca646b8e382cd0f
jdeepika <deepuram918@gmail.com>
2016-11-22 19:51:09 -0500

    done the assignment
    Please review and provide me the feedback.
    
    Thanks

---
+++ javascript/11_22_2016/deepika/primitives.js
@@ -0,0 +1,34 @@
+
+
+console.log("welcome to primitive data types ");
+
+var stringdatatype="string data type";
+
+console.log(stringdatatype);
+
+var intdatatype=10;
+
+console.log(intdatatype);
+
+var doubledatatype =10.22;
+
+console.log(doubledatatype);
+
+var istrue = true;
+
+console.log("istrue : "+ istrue);
+
+var isfalse = false;
+
+console.log("isfalse : "+isfalse);
+
+var undefinedVar =undefined;
+
+console.log("undefined value : "+ undefinedVar);
+
+
+var nullvar = null;
+
+console.log(" Null value : "+ nullvar);
+
+
